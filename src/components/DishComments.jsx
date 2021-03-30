1976cba3c2e0c4d1312ea149528a5abf00c7d53f
Silviu Alexandru Murgu <silviu.murgu@yahoo.com>
2021-03-30 17:33:53 +0300

    converted to class based

--- src/components/DishComments.jsx
+++ src/components/DishComments.jsx
@@ -1,18 +1,28 @@
+import React from 'react'
 import { ListGroup } from 'react-bootstrap'
 
-const DishComments = (props) =>
-    props.selectedDish
-        ? <ListGroup>
-            <h2>Comments for {props.selectedDish.name}</h2>
-            {
-                props.selectedDish.comments.map(c => (
-                    <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
-                ))
-            }
-        </ListGroup>
-        : <h1>NOTHING YET</h1>
+class DishComments extends React.Component{
+    
+    render(){
+        return (
+           
+            <>
+                {this.props.selectedDish
+                 ? <ListGroup>
+                    <h2>Comments for {this.props.selectedDish.name}</h2>
+                    {
+                        this.props.selectedDish.comments.map(c => (
+                            <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
+                        ))
+                 }
+                </ListGroup>
+                : <h1>NOTHING YET</h1>}
+            </>
+        )
+    }
+    
 
-
+    }
 export default DishComments
 
 // mapping props.selectedDish.comments
